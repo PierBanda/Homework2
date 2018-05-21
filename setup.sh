c038d1e2d16372f440749739bf25d273c1dd4569
PierBanda <pierluigi.bandino@studenti.univr.it>
2018-05-21 14:03:57 +0200

    aggiorno launcher e setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
-cp Aula_H_final.world ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models/.
+#!/bin/sh
+
+cp Aula_H.world ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/worlds/.
 cp Homework2.launch ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/launch/.
-cp -r aula_final ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models/.
+cp -r aula_H ~/.gazebo/models/.
 
-echo Select your turtlebot model
-echo export TURTLEBOT3_MODEL=waffle
-
-echo Launch cyber_lab with turtlebot:
-echo roslaunch turtlebot3_gazebo Homework2.launch
-
-echo 
-echo Then launch Turtlebot autonomous navigation:
-echo roslaunch turtlebot3_gazebo turtlebot3_simulation.launch
+echo 'if everything is ok'
+echo 'open 3 new terminals and execute:
+'
+echo 'export TURTLEBOT3_MODEL=waffle && roslaunch turtlebot3_gazebo Homework2.launch
+'
+echo 'export TURTLEBOT3_MODEL=waffle && roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=$HOME/catkin_ws/src/Homework2/map/map.yaml
+'
+echo 'export TURTLEBOT3_MODEL=waffle && rosrun rviz rviz -d `rospack find turtlebot3_navigation`/rviz/turtlebot3_nav.rviz
+'
